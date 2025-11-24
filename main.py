6ff647b4a68a6b8a3258ce494f76a54ff7b7358f
teams-ujjaiwal-resourcess-lab <ffg288844@gmail.com>
2025-11-24 17:15:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -310,4 +310,6 @@ def get_single_response():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)

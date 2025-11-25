ed3958451f77562fb3cfa88a3ed36e6cc53a674d
teams-ujjaiwal-resourcess-lab <ffg288844@gmail.com>
2025-11-25 07:32:15 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -309,7 +309,5 @@ def get_single_response():
         return jsonify({"error": str(e)}), 500
 
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+iif __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
